fa46b6a13007fbf9f7224ff67b1c950c0f5df7d0
trymycode <imitasingha1994@gmail.com>
2019-01-16 18:58:15 +0530

    added authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase';
 import { Observer, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$: Observable<firebase.User>;
-  constructor(private afAuth: AngularFireAuth) { 
+
+  constructor(
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
   login() {
+    let returnUrl =
+    this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider());
   }
 
