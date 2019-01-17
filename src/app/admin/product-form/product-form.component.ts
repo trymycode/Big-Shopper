f902319f4c2e254b82c3a408ee9856e8667615ca
trymycode <imitasingha1994@gmail.com>
2019-01-17 17:14:21 +0530

    added product category options

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-
-  constructor() { }
+  categories$;
+  constructor( categoryService: CategoryService) { 
+    this.categories$ = categoryService.getCategories();
+  }
 
   ngOnInit() {
   }
