9bfca9e9d7b581bbfddfa89d7ee186e53d4d4206
trymycode <imitasingha1994@gmail.com>
2019-01-18 10:43:14 +0530

    added product edit details

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,27 +1,42 @@
 import { ProductServiceService } from './../../product-service.service';
 import { CategoryService } from './../../category.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { Component} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
+  public id: string;
+  public product:any = [];
+  public subscriber: Subscription;
   categories$;
   constructor( 
     private router: Router,
+    private route: ActivatedRoute,
     private categoryService: CategoryService, 
-    private productService:ProductServiceService )
+    private productService:ProductServiceService
+    )
     { 
      this.categories$ =  categoryService.getCategories();
+
+     this.id = this.route.snapshot.paramMap.get('id');
+     if(this.id) this.productService.getProduct(this.id).pipe(
+       take(1)
+     ).subscribe(
+       data => {
+         this.product = data
+       }
+     )
     }
+
   save(product){
     this.productService.create(product);
     this.router.navigate(['/admin/products']);
   }
-  ngOnInit() {
-  }
+ 
 
 }
--- src/app/product-service.service.ts
+++ src/app/product-service.service.ts
@@ -12,4 +12,8 @@ export class ProductServiceService {
   getAllProducts() {
     return this.db.list('/products');
   }
+  getProduct(id){
+    return this.db.object('/products/'+ id)
+    .valueChanges();
+  }
 }
