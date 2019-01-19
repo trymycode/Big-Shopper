56a3718d0f798853c683650044cf3a9de4293128
trymycode <imitasingha1994@gmail.com>
2019-01-20 00:13:20 +0530

    added shopping-cart functions

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -7,10 +7,25 @@ import { Injectable } from '@angular/core';
 export class ShoppingCartService {
 
   constructor( private db: AngularFireDatabase ) { }
-  create(){
+  public create(){
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     })
+  }
+
+  private getCart(cartId: string){
+    return this.db.object('/shopping-carts/'+ cartId);
+  }
+
+  private async getOrCreateCart(){
+    let cartId = localStorage.getItem('cartId');
+    if(!cartId){
+     let result = await this.create();
+     localStorage.setItem('cartId', result.key);
+     return this.getCart(result.key);
+      
+    } 
+     return this.getCart(cartId);
   }
 
 }
